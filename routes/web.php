e958ec36c3fd32cb2f35793edca0a6a217ca9685
Alek Marcondes <89283892+kallemback@users.noreply.github.com>
2023-06-26 21:16:00 -0300

    concluido

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 //rotas
 
-Route::get('/', 'teste@index');
+Route::get('/', function(){
+    return view(view: 'site.home');
+})->name('site.home');
+
+Route::get('/cursos', function(){
+    return view(view: 'site.courses');
+})->name('site.courses');
+
+Route::get('/contato', function(){
+    return view(view: 'site.contact');
+})->name('site.contact');
